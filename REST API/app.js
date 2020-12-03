b1d95bab81494a5d08df3b6f26771c0fbc75d34d
vasilzahariev <vasilzahariev1707@abv.bg>
2020-12-03 17:33:22 +0200

    feat: Added connection to DB

--- REST API/app.js
+++ REST API/app.js
@@ -7,7 +7,20 @@ const config = require('./config/config')[env];
 const app = express();
 
 // Setting up connection with the database
-// TODO
+mongoose.connect(config.dbConnectionUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log(err);
 
-// Making the app listen on a specific port
+        throw err;
+    }
+
+    console.log('Database is setup and running!');
+});
+
+//require('./config/express')(app);
+
+// Binds and listens for connections on the specific port
 app.listen(config.PORT, console.log(`REST API is listening on http://localhost:${config.PORT}`));
--- REST API/config/config.js
+++ REST API/config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     'development': {
         PORT: process.env.PORT || 3001,
-        privateKey: 'DEVCOM-RESTAPI-PrivateKey'
+        privateKey: 'DEVCOM-RESTAPI-PrivateKey',
+        dbConnectionUrl: `mongodb+srv://ProjectDevComDBUser:${process.env.DB_PASSWORD}@devcom.rdkts.mongodb.net/DevCom?retryWrites=true&w=majority`
     },
     'production': {}
 }
